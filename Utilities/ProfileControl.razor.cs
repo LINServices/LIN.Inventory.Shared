1db60eb5dada5ebf681dbc6a9710f49a413cd69a
Alexander Giraldo <giraldojhong4@gmail.com>
2025-02-18 19:53:35 -0500

    Actualiza idioma y mejora robustez en componentes
    - Cambia el idioma del documento HTML a "es" en `App.razor`.
    - Añade puntos al final de las descripciones de funciones en `Flowbite.js`.
    - Implementa verificaciones nulas en `ProfileControl.razor` para evitar errores.
    - Elimina propiedades de la clase `Device` en `Device.cs`, simplificando la clase.
    - Mejora el manejo de texto en `ProfileControl.razor` para casos nulos.

--- Utilities/ProfileControl.razor.cs
+++ Utilities/ProfileControl.razor.cs
@@ -9,20 +9,17 @@ public partial class ProfileControl
     [Parameter]
     public Types.Cloud.Identity.Abstracts.SessionModel<Types.Inventory.Models.ProfileModel>? Model { get; set; }
 
-
     /// <summary>
     /// Evento al hacer click.
     /// </summary>
     [Parameter]
     public Action<Types.Cloud.Identity.Abstracts.SessionModel<Types.Inventory.Models.ProfileModel>?>? OnClick { get; set; }
 
-
     /// <summary>
     /// Estado.
     /// </summary>
     [Parameter]
     public bool State { get; set; }
-
 
     /// <summary>
     /// Enviar el evento.
