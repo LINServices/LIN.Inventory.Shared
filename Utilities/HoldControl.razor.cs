83f03111acdd6357594530b4c490eb2e30918df1
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-13 17:16:20 -0500

    reservas

---
+++ Utilities/HoldControl.razor.cs
@@ -0,0 +1,30 @@
+﻿namespace LIN.Inventory.Shared.Utilities;
+
+public partial class HoldControl
+{
+
+    /// <summary>
+    /// Modelo del producto.
+    /// </summary>
+    [Parameter]
+    public HoldModel? Model { get; set; }
+
+
+
+    /// <summary>
+    /// Evento al hacer click.
+    /// </summary>
+    [Parameter]
+    public Action<HoldModel?>? OnClick { get; set; }
+
+
+
+    /// <summary>
+    /// Enviar el evento.
+    /// </summary>
+    private void SendEvent()
+    {
+        OnClick?.Invoke(Model);
+    }
+
+}
